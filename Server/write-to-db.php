99d4386742263cf5d83fe68c6fe2d35db9b3292a
motoki <krkrmfmf@outlook.jp>
2022-07-02 23:34:03 +0900

    DBへの書き込みを追加

--- Server/write-to-db.php
+++ Server/write-to-db.php
@@ -1,5 +1,25 @@
 <?php
 
-$json = file_get_contents('php://input');
+$configJson = file_get_contents('./config.json');
+$config = json_decode($configJson);
 
-var_dump($json);
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+$mysqli = new mysqli($config->host, $config->user, $config->password, $config->db);
+
+if (mysqli_connect_error())
+{
+    die("can not connect db.");
+}
+
+$datetime = date('Y-m-d H:m:s');
+
+$query = "INSERT INTO temp(`datetime`, `temp`) VALUES (`$datetime`, `$data->temp`);";
+
+var_dump($query);
+
+$stmt = $mysqli->prepare($query);
+$result = $stmt->execute();
+
+var_dump($stmt);
