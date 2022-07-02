45a3e53025cb9eb6d6c2a1842cd4d371692cfc81
motoki <krkrmfmf@outlook.jp>
2022-07-03 03:55:26 +0900

    クラス名変更 データのグラフ表示用HTML追加 データ取得用API追加

--- Server/index.php
+++ Server/index.php
@@ -1,20 +1,31 @@
 <?php
 
-require_once('./lib/WriteToDb.php');
+require_once('./lib/AquariumTempDb.php');
 require_once('./lib/TempData.php');
 
-$writeToDb = new WriteToDb();
+$aquariumTempDb = new AquariumTempDb();
 
-$json = file_get_contents('php://input');
-$tempData = new TempData($json);
 
-$result = $writeToDb->Insert($tempData->temp);
-
-if ($result == true)
+if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    echo "OK";
+    $json = file_get_contents('php://input');
+    $tempData = new TempData($json);
+
+    $result = $aquariumTempDb->Insert($tempData->temp);
+
+    if ($result == true)
+    {
+        echo "OK";
+    }
+    else
+    {
+        echo "NG";
+    }
 }
 else
 {
-    echo "NG";
+    header("Content-Type: application/json; charset=utf-8");
+    $data = $aquariumTempDb->Select();
+    $json = json_encode($data);
+    echo $json;
 }
