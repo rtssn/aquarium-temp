cea13f5817048223400cce6454dd79b3164acc43
motoki <krkrmfmf@outlook.jp>
2022-07-03 00:47:17 +0900

    RSSI表示追加 POST間隔を定数化

--- M5StickC/DS18B20.ino
+++ M5StickC/DS18B20.ino
@@ -5,22 +5,44 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+/**
+ * 1-Wireのピン
+ */
 #define ONE_WIRE_BUS 33
 
 /**
  * POST間隔(ms)
  */
-#define POST_INTERVAL = 5000;
+#define POST_INTERVAL 5000
 
 Preferences preferences;
 char ssid[33];
 char password[65];
 float tempC = 0;
 
-WiFiServer server(80);
 HTTPClient http;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+
+void setup()
+{
+    M5.begin();
+    Serial.begin(115200);
+    delay(500);
+
+    ConnectWiFi();
+
+    M5.Axp.ScreenBreath(7);
+    M5.Lcd.setRotation(3);
+    delay(500);
+}
+
+void loop()
+{
+    GetTemp();
+    PostData();
+    ShowDisplay();
+}
 
 /**
  * Wi-Fi接続
@@ -46,38 +68,18 @@ void ConnectWiFi()
     Serial.println(WiFi.localIP());
 }
 
-void setup()
-{
-    M5.begin();
-    Serial.begin(115200);
-    delay(500);
-
-    ConnectWiFi();
-
-    M5.Lcd.setRotation(3);
-    M5.Lcd.setTextFont(4);
-    M5.lcd.println("IP Address:");
-    M5.Lcd.println(WiFi.localIP());
-
-    server.begin();
-}
-
-void loop()
-{
-    GetTemp();
-    PostData();
-    ShowDisplay();
-}
-
 /**
  * ディスプレイ表示
  */
 void ShowDisplay()
 {
     M5.lcd.setCursor(0, 0);
-    M5.lcd.setTextFont(4);
+    M5.lcd.setTextFont(2);
     M5.lcd.println("IP Address:");
     M5.Lcd.println(WiFi.localIP());
+    M5.lcd.print("RSSI:");
+    M5.Lcd.println(WiFi.RSSI());
+    M5.lcd.setTextFont(4);
     m5.Lcd.print("temp: ");
     M5.lcd.print(tempC);
 }
@@ -90,7 +92,9 @@ unsigned long postTime = 0;
 void PostData()
 {
     postTime = millis();
-    if (postTime - postLast > 5000 || isFirst == true)
+    unsigned long diff = postTime - postLast;
+
+    if (diff > POST_INTERVAL || isFirst == true)
     {
         String data = "{\"temp\": " + String(tempC) + "}";
 
@@ -110,18 +114,16 @@ void PostData()
  */
 void GetTemp()
 {
-    // Serial.print("Requesting temperatures...");
-    sensors.requestTemperatures(); // Send the command to get temperatures
-    // Serial.println("DONE");
+    sensors.requestTemperatures();
     tempC = sensors.getTempCByIndex(0);
 
     if (tempC != DEVICE_DISCONNECTED_C)
     {
-        // Serial.print("Temperature for the device 1 (index 0) is: ");
-        // Serial.println(tempC);
+        Serial.print("Temperature for the device 1 (index 0) is: ");
+        Serial.println(tempC);
     }
     else
     {
-        // Serial.println("Error: Could not read temperature data");
+        Serial.println("Error: Could not read temperature data");
     }
 }
