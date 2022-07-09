891707be825fb266dae2d464f698b62271201523
motoki <krkrmfmf@outlook.jp>
2022-07-09 23:04:12 +0900

    M5-Stampに変更 ファンのPWM制御を追加 ファン回転集取得を追加

--- Arduino/DS18B20.ino
+++ Arduino/DS18B20.ino
@@ -1,6 +1,5 @@
 #include <DallasTemperature.h>
 #include <HTTPClient.h>
-#include <M5StickC.h>
 #include <OneWire.h>
 #include <Preferences.h>
 #include <WiFi.h>
@@ -8,52 +7,71 @@
 /**
  * 1-Wireのピン
  */
-#define ONE_WIRE_BUS 33
+#define ONE_WIRE_BUS 5
 
 /**
  * POST間隔(ms)
  */
 #define POST_INTERVAL 60000
 
-Preferences preferences;
-char ssid[33];
-char password[65];
-float tempC = 0;
-float cpuTemp = 0;
-bool getTempStatus = false;
-
 HTTPClient http;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+float tempC;
+bool getTempStatus;
+
+unsigned long lastPulseTime;
+unsigned long pulseInterval;
+unsigned long lastUpdateTime;
+uint16_t rpm;
+
+const double PWM_Hz = 2000;  // PWM周波数
+const uint8_t PWM_level = 8; // PWM分解能 16bit(1～256)
+const uint8_t PWM_CH = 1;    // チャンネル
+
+//温度取得用タイマーカウンタ
+unsigned long postLast = 0;
 
 void setup()
 {
-  M5.begin();
-  Serial.begin(115200);
-  delay(500);
+    Serial.begin(115200);
+    delay(5000);
 
-  ConnectWiFi();
+    ConnectWiFi();
 
-  M5.Axp.ScreenBreath(8);
-  M5.Lcd.setRotation(3);
+    FirstGetTemp();
 
-  FirstGetTemp();
+    lastPulseTime = 0;
+    pulseInterval = 0;
+
+    pinMode(6, INPUT);
+    attachInterrupt(6, SenseRotation, FALLING); // 0 = D2, FALLING = 立ち下がり
+
+    pinMode(8, OUTPUT);
+
+    // チャンネルと周波数の分解能を設定
+    ledcSetup(PWM_CH, PWM_Hz, PWM_level);
+    // モータのピンとチャンネルの設定
+    ledcAttachPin(8, PWM_CH);
 }
 
 void loop()
 {
-  GetTemp();
 
-  if (getTempStatus == true)
-  {
-    PostDataTimer();
-  }
+    GetTempTimer();
+}
 
-  ShowDisplay();
+void GetFanRpm()
+{
+    unsigned long cur = millis();
+    if (cur - lastUpdateTime > 1000)
+    { // 60ms間隔でLEDを更新
 
-  CheckConnectWiFi();
+        rpm = 60000000 / (pulseInterval * 2); // RPMを求める
+        Serial.println(rpm);
 
-  cpuTemp = temperatureRead();
+        lastUpdateTime = cur;
+    }
 }
 
 /**
@@ -61,14 +79,13 @@ void loop()
  */
 void FirstGetTemp()
 {
-  delay(2000);
-  GetTemp();
-  ShowDisplay();
+    delay(2000);
+    GetTemp();
 
-  if (getTempStatus == true)
-  {
-    PostData();
-  }
+    if (getTempStatus == true)
+    {
+        PostData();
+    }
 }
 
 /**
@@ -76,23 +93,27 @@ void FirstGetTemp()
  */
 void ConnectWiFi()
 {
-  // Wi-Fiアクセスポイント情報取得
-  preferences.begin("AP-info", true);
-  preferences.getString("ssid", ssid, sizeof(ssid));
-  preferences.getString("pass", password, sizeof(password));
-  preferences.end();
+    Preferences preferences;
+    char ssid[33];
+    char password[65];
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
+    // Wi-Fiアクセスポイント情報取得
+    preferences.begin("AP-info", true);
+    preferences.getString("ssid", ssid, sizeof(ssid));
+    preferences.getString("pass", password, sizeof(password));
+    preferences.end();
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 /**
@@ -101,50 +122,36 @@ void ConnectWiFi()
 void CheckConnectWiFi()
 {
 
-  if (WiFi.status() != WL_DISCONNECTED)
-  {
-    delay(500);
+    if (WiFi.status() != WL_DISCONNECTED)
+    {
+        delay(500);
 
-    Serial.print("WiFi dicconnected.");
+        Serial.print("WiFi dicconnected.");
 
-    delay(2000);
-    ConnectWiFi();
-  }
+        delay(2000);
+        ConnectWiFi();
+    }
 }
 
 /**
- * ディスプレイ表示処理です。
+ * 温度取得のタイマー関数です。
  */
-void ShowDisplay()
+void GetTempTimer()
 {
-  M5.lcd.setCursor(0, 0);
-  M5.lcd.setTextFont(2);
-  M5.lcd.println("IP Address:");
-  M5.Lcd.println(WiFi.localIP());
-  M5.lcd.print("RSSI: ");
-  M5.Lcd.println(WiFi.RSSI());
-  M5.lcd.setTextFont(4);
-  m5.Lcd.print("temp: ");
-  M5.lcd.print(tempC);
-}
+    unsigned long postTime = millis();
+    unsigned long diff = postTime - postLast;
 
-//送信用カウンタ
-unsigned long postLast = 0;
+    if (diff > POST_INTERVAL)
+    {
+        GetTemp();
 
-/**
- * 温度データ送信のタイマー関数です。
- */
-void PostDataTimer()
-{
-  unsigned long postTime = millis();
-  unsigned long diff = postTime - postLast;
+        if (getTempStatus == true)
+        {
+            PostData();
+        }
 
-  if (diff > POST_INTERVAL)
-  {
-    PostData();
-
-    postLast = postTime;
-  }
+        postLast = postTime;
+    }
 }
 
 /**
@@ -152,18 +159,20 @@ void PostDataTimer()
  */
 void PostData()
 {
-  String tempCString = "\"temp\":" + String(tempC);
-  String cpuTempString = "\"cpuTemp\": " + String(cpuTemp);
-  String ipAddressString = "\"ipAddress\": " + String(WiFi.localIP());
+    String tempCString = "\"temp\": " + String(tempC);
+    IPAddress ipAddress = WiFi.localIP();
+    String ipAddressString = "\"ipAddress\": \"" + String(ipAddress[0]) + "." + String(ipAddress[1]) + "." +
+                             String(ipAddress[2]) + "." + String(ipAddress[3]) + "\"";
 
-  String data =
-      "{" + tempCString + "," + cpuTempString + "," + ipAddressString + "}";
+    String rpmString = "\"fanRpm\": " + String(rpm);
 
-  http.begin("http://192.168.11.100/test/aqua/api/");
-  http.addHeader("Content-Type", "application/json");
-  http.POST(data);
-  Serial.println(http.getString());
-  http.end();
+    String data = "{" + tempCString + ", " + rpmString + ", " + ipAddressString + "}";
+
+    http.begin("http://192.168.11.100/test/aqua/api/");
+    http.addHeader("Content-Type", "application/json");
+    http.POST(data);
+    Serial.println(http.getString());
+    http.end();
 }
 
 /**
@@ -171,18 +180,27 @@ void PostData()
  */
 void GetTemp()
 {
-  sensors.requestTemperatures();
-  tempC = sensors.getTempCByIndex(0);
+    sensors.requestTemperatures();
+    tempC = sensors.getTempCByIndex(0);
 
-  if (tempC != DEVICE_DISCONNECTED_C)
-  {
-    Serial.print("Temperature for the device 1 (index 0) is: ");
-    Serial.println(tempC);
-    getTempStatus = true;
-  }
-  else
-  {
-    Serial.println("Error: Could not read temperature data");
-    getTempStatus = false;
-  }
+    if (tempC != DEVICE_DISCONNECTED_C)
+    {
+        Serial.print("Temperature for the device 1 (index 0) is: ");
+        Serial.println(tempC);
+        getTempStatus = true;
+    }
+    else
+    {
+        Serial.println("Error: Could not read temperature data");
+        getTempStatus = false;
+    }
+}
+
+// 回転パルス立ち下がりエッジ検出用
+void SenseRotation(void)
+{
+    unsigned long cur = micros();
+    unsigned long dif = cur - lastPulseTime;                             // 前回のエッジとの差分
+    pulseInterval = (pulseInterval - (pulseInterval >> 2)) + (dif >> 2); // 滑らかに
+    lastPulseTime = cur;
 }
