d25f9663735ddc9896976f5fbe82c5dd8bae56ed
motoki <krkrmfmf@outlook.jp>
2022-07-17 03:15:42 +0900

    センサーを二個に変更

--- Arduino/DS18B20.ino
+++ Arduino/DS18B20.ino
@@ -14,20 +14,13 @@
  */
 #define POST_INTERVAL 60000
 
+#define FAN_CONTROL_PIN 6
+
 HTTPClient http;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-float tempC;
-bool getTempStatus;
 
-unsigned long lastPulseTime;
-unsigned long pulseInterval;
-unsigned long lastUpdateTime;
-uint16_t rpm;
-
-const double PWM_Hz = 2000;  // PWM周波数
-const uint8_t PWM_level = 8; // PWM分解能 16bit(1～256)
-const uint8_t PWM_CH = 1;    // チャンネル
+bool getTempStatus = false;
 
 //温度取得用タイマーカウンタ
 unsigned long postLast = 0;
@@ -41,37 +34,13 @@ void setup()
 
     FirstGetTemp();
 
-    lastPulseTime = 0;
-    pulseInterval = 0;
-
-    pinMode(6, INPUT);
-    attachInterrupt(6, SenseRotation, FALLING); // 0 = D2, FALLING = 立ち下がり
-
-    pinMode(8, OUTPUT);
-
-    // チャンネルと周波数の分解能を設定
-    ledcSetup(PWM_CH, PWM_Hz, PWM_level);
-    // モータのピンとチャンネルの設定
-    ledcAttachPin(8, PWM_CH);
+    pinMode(FAN_CONTROL_PIN, OUTPUT);
 }
 
 void loop()
 {
-
     GetTempTimer();
-}
-
-void GetFanRpm()
-{
-    unsigned long cur = millis();
-    if (cur - lastUpdateTime > 1000)
-    { // 60ms間隔でLEDを更新
-
-        rpm = 60000000 / (pulseInterval * 2); // RPMを求める
-        Serial.println(rpm);
-
-        lastUpdateTime = cur;
-    }
+    CheckConnectWiFi();
 }
 
 /**
@@ -80,11 +49,115 @@ void GetFanRpm()
 void FirstGetTemp()
 {
     delay(2000);
-    GetTemp();
+    float sensor1_temp = GetTemp(0);
+    float sensor2_temp = GetTemp(1);
 
     if (getTempStatus == true)
     {
-        PostData();
+        bool isFanOn = IsFanOn(sensor1_temp);
+
+        FanControl(isFanOn);
+        PostData(sensor1_temp, sensor2_temp, isFanOn);
+    }
+}
+
+/**
+ * 温度取得のタイマー関数です。
+ */
+void GetTempTimer()
+{
+    unsigned long postTime = millis();
+    unsigned long diff = postTime - postLast;
+
+    if (diff > POST_INTERVAL)
+    {
+        float sensor1_temp = GetTemp(0);
+        float sensor2_temp = GetTemp(1);
+
+        if (getTempStatus == true)
+        {
+            bool isFanOn = IsFanOn(sensor1_temp);
+
+            FanControl(isFanOn);
+            PostData(sensor1_temp, sensor2_temp, isFanOn);
+        }
+
+        postLast = postTime;
+    }
+}
+
+bool IsFanOn(float temp)
+{
+    bool ret = false;
+
+    if (temp >= 26)
+    {
+        ret = true;
+    }
+
+    return ret;
+}
+
+/**
+ * 取得した温度を送信します。
+ */
+void PostData(float sensor1_temp, float sensor2_temp, bool isFanOn)
+{
+    String sensors = "[";
+    sensors = sensors + "{\"temp\": " + String(sensor1_temp) + "},";
+    sensors = sensors + "{\"temp\": " + String(sensor2_temp) + "}";
+    sensors = sensors + "]";
+
+    String sensorString = "\"sensors\": " + sensors;
+    IPAddress ipAddress = WiFi.localIP();
+    String ipAddressString = "\"ipAddress\": \"" + String(ipAddress[0]) + "." + String(ipAddress[1]) + "." +
+                             String(ipAddress[2]) + "." + String(ipAddress[3]) + "\"";
+
+    String isFanOnString = "\"isFanOn\": " + String(isFanOn);
+
+    String data = "{" + sensorString + ", " + isFanOnString + ", " + ipAddressString + "}";
+
+    Serial.println(data);
+
+    http.begin("http://192.168.11.100/test/aqua/api/");
+    http.addHeader("Content-Type", "application/json");
+    http.POST(data);
+    Serial.println(http.getString());
+    http.end();
+}
+
+/**
+ * DS18B20から温度取得
+ */
+float GetTemp(uint8_t index)
+{
+    sensors.requestTemperatures();
+
+    float tempC = sensors.getTempCByIndex(index);
+    if (tempC != DEVICE_DISCONNECTED_C)
+    {
+        Serial.print("Temperature for the device 1 (index " + String(index) + ") is: ");
+        Serial.println(tempC);
+        getTempStatus = true;
+    }
+    else
+    {
+        Serial.println("Error: Could not read temperature data");
+        getTempStatus = false;
+    }
+
+    return tempC;
+}
+
+void FanControl(bool isFanOn)
+{
+    if (isFanOn == true)
+    {
+        digitalWrite(FAN_CONTROL_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(FAN_CONTROL_PIN, LOW);
     }
 }
 
@@ -121,86 +194,13 @@ void ConnectWiFi()
  */
 void CheckConnectWiFi()
 {
-
-    if (WiFi.status() != WL_DISCONNECTED)
+    if (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
 
         Serial.print("WiFi dicconnected.");
 
-        delay(2000);
+        delay(10000);
         ConnectWiFi();
     }
-}
-
-/**
- * 温度取得のタイマー関数です。
- */
-void GetTempTimer()
-{
-    unsigned long postTime = millis();
-    unsigned long diff = postTime - postLast;
-
-    if (diff > POST_INTERVAL)
-    {
-        GetTemp();
-
-        if (getTempStatus == true)
-        {
-            PostData();
-        }
-
-        postLast = postTime;
-    }
-}
-
-/**
- * 取得した温度を送信します。
- */
-void PostData()
-{
-    String tempCString = "\"temp\": " + String(tempC);
-    IPAddress ipAddress = WiFi.localIP();
-    String ipAddressString = "\"ipAddress\": \"" + String(ipAddress[0]) + "." + String(ipAddress[1]) + "." +
-                             String(ipAddress[2]) + "." + String(ipAddress[3]) + "\"";
-
-    String rpmString = "\"fanRpm\": " + String(rpm);
-
-    String data = "{" + tempCString + ", " + rpmString + ", " + ipAddressString + "}";
-
-    http.begin("http://192.168.11.100/test/aqua/api/");
-    http.addHeader("Content-Type", "application/json");
-    http.POST(data);
-    Serial.println(http.getString());
-    http.end();
-}
-
-/**
- * DS18B20から温度取得
- */
-void GetTemp()
-{
-    sensors.requestTemperatures();
-    tempC = sensors.getTempCByIndex(0);
-
-    if (tempC != DEVICE_DISCONNECTED_C)
-    {
-        Serial.print("Temperature for the device 1 (index 0) is: ");
-        Serial.println(tempC);
-        getTempStatus = true;
-    }
-    else
-    {
-        Serial.println("Error: Could not read temperature data");
-        getTempStatus = false;
-    }
-}
-
-// 回転パルス立ち下がりエッジ検出用
-void SenseRotation(void)
-{
-    unsigned long cur = micros();
-    unsigned long dif = cur - lastPulseTime;                             // 前回のエッジとの差分
-    pulseInterval = (pulseInterval - (pulseInterval >> 2)) + (dif >> 2); // 滑らかに
-    lastPulseTime = cur;
 }
