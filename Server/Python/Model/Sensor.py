e206f5c246d12ad2498e9249e532e6059980facd
motoki <krkrmfmf@outlook.jp>
2022-08-07 15:09:02 +0900

    pythonのコードを削除

--- Server/Python/Model/Sensor.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Sensor(BaseModel):
-    address: str
-    temp: float
--- Server/Python/Model/TempData.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel
-
-
-class TempData(BaseModel):
-    datetime: datetime
-    sensor1Temp: float
-    sensor2Temp: float
-    isFanOn: int
